225c63ddb7ad9c57c18d17ce4e987686e62b2ad9
Jonatas <jonatasolmartins@outlook.com>
2022-11-08 16:35:44 -0300

    Cleanup code

--- Design.Pattern.Command.Api/ReceiversExtension.cs
+++ Design.Pattern.Command.Api/ReceiversExtension.cs
@@ -8,16 +8,9 @@ public static class ReceiversExtension
 {
     public static IServiceCollection AddReceivers(this IServiceCollection services, params Type[] assemnlyTypes)
     {
-        //var receiversDict = Dictionary<Type, Type>();
         foreach (var types in assemnlyTypes)
         {
             var assembly = types.Assembly;
-            // var commands = assembly.GetTypes().Where(x =>
-            // {
-            //     var requestImplentation = x.GetInterfaces().Where(y => y.IsGenericType)
-            //         .Any(z => z.GetGenericTypeDefinition() == typeof(ICommand<>));
-            //     return !x.IsInterface && !x.IsAbstract && requestImplentation;
-            // }).ToList();
 
             var receivers = assembly.GetTypes().Where(x =>
             {
@@ -26,12 +19,6 @@ public static class ReceiversExtension
                 return !x.IsInterface && !x.IsAbstract && requestImplentation;
             }).ToList();
             
-           //commands.ForEach(cmd =>
-           //{
-           //    receiversDict[cmd] =
-           //        receivers.SingleOrDefault(rec => cmd == rec.GetInterface("IReceiver`2")!.GetGenericArguments()[0]);
-           //});
-
            var serviceDescriptor = receivers.Select(x => new ServiceDescriptor(x, x, ServiceLifetime.Scoped));
            services.TryAdd(serviceDescriptor);
         }
